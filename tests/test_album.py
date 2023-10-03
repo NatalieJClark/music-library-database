d564d7fa535bb1267cae02fa8ed3dca88f83e8ee
Natalie Clark <nj_clark@hotmail.co.uk>
2023-10-03 20:47:50 +0100

    Add Album unit tests

--- tests/test_album.py
+++ tests/test_album.py
@@ -1,5 +1,6 @@
 from lib.album import Album
 
+
 """
 Album constructs with id, title, release_year and album_id
 """
@@ -9,3 +10,19 @@ def test_album_constructs():
     assert album.title == "OK Computer"
     assert album.release_year == 1997
     assert album.artist_id == 3
+
+"""
+We can compare two identical albums
+And have them be equal
+"""
+def test_albums_are_equal():
+    album1 = Album(1, "Test Album", 1995, 2)
+    album2 = Album(1, "Test Album", 1995, 2)
+    assert album1 == album2
+
+"""
+We can format albums to strings nicely
+"""
+def test_artists_format_nicely():
+    album = Album(1, "Test Album", 1995, 2)
+    assert str(album) == "Album(1, Test Album, 1995, 2)"
