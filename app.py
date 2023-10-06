53ca7925c7d591bfc892d4cf3d82decd74f5a5ee
Natalie Clark <nj_clark@hotmail.co.uk>
2023-10-06 16:17:13 +0100

    Add Application class

--- app.py
+++ app.py
@@ -2,24 +2,28 @@ from lib.database_connection import DatabaseConnection
 from lib.artist_repository import ArtistRepository
 from lib.album_repository import AlbumRepository
 
+class Application():
+    def __init__(self):
+        self._connection = DatabaseConnection()
+        self._connection.connect()
+        self._connection.seed("seeds/music_library.sql")
 
-# Connect to the database
-connection = DatabaseConnection()
-connection.connect()
+    def run(self):
+        print("Welcome to the music library manager!")
+        choice = input("What would you like to do?\n 1 - List all albums\n 2 - List all artists\nEnter your choice: ")
+        album_repository = AlbumRepository(self._connection)
+        artist_repository = ArtistRepository(self._connection)
+        if choice == "1":
+            print("\nHere is the list of albums:")
+            for album in album_repository.all():
+                print(album)
+        elif choice == "2":
+            print("\nHere is the list of artists:")
+            for artist in artist_repository.all():
+                print(artist)
+        else:
+            print("Invalid choice")
 
-# Seed with some seed data
-connection.seed("seeds/music_library.sql")
-
-# Retrieve all artists
-artist_repository = ArtistRepository(connection)
-for artist in artist_repository.all():
-    print(artist)
-
-# Retrieve all albums
-album_repository = AlbumRepository(connection)
-for album in album_repository.all():
-    print(album)
-
-# Retrieve the ablbum with id = 1
-album_repository = AlbumRepository(connection)
-print(album_repository.find(1))
+if __name__ == '__main__':
+    app = Application()
+    app.run()
