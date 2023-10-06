d2bf071550b97387c644e8bc05aca953f5a93ccf
Natalie Clark <nj_clark@hotmail.co.uk>
2023-10-06 14:46:26 +0100

    Add integration tests for create & delete methods

--- tests/test_album_repository.py
+++ tests/test_album_repository.py
@@ -33,3 +33,53 @@ def test_find(db_connection):
     repository = AlbumRepository(db_connection)
     result = repository.find(2)
     assert result == Album(2, 'Surfer Rosa', 1988, 1)
+
+"""
+When we call AlbumRepository#create
+With an album object
+It returns None
+And the created row is reflected in the list when we call ALbumRepository#all
+"""
+def test_create_record_in_albums(db_connection):
+    db_connection.seed("seeds/music_library.sql")
+    repository = AlbumRepository(db_connection)
+    album = Album(None, "Evermore", 2020, 3)
+    assert repository.create(album) == None
+    assert repository.all() == [
+        Album(1, 'Doolittle', 1989, 1),
+        Album(2, 'Surfer Rosa', 1988, 1),
+        Album(3, 'Waterloo', 1974, 2),
+        Album(4, 'Super Trouper', 1980, 2),
+        Album(5, 'Bossanova', 1990, 1),
+        Album(6, 'Lover', 2019, 3),
+        Album(7, 'Folklore', 2020, 3),
+        Album(8, 'I Put a Spell on You', 1965, 4),
+        Album(9, 'Baltimore', 1978, 4),
+        Album(10, 'Here Comes the Sun', 1971, 4),
+        Album(11, 'Fodder on My Wings', 1982, 4),
+        Album(12, 'Ring Ring', 1973, 2),
+        Album(13, 'Evermore', 2020, 3)
+    ]
+
+"""
+When we call AlbumRepository#delete
+With an id
+It is reflected in the list when we call AlbumRepository#all
+"""
+def test_delete_with_id(db_connection):
+    db_connection.seed("seeds/music_library.sql")
+    repository = AlbumRepository(db_connection)
+    assert repository.delete(3) == None
+    assert repository.all() == [
+        Album(1, 'Doolittle', 1989, 1),
+        Album(2, 'Surfer Rosa', 1988, 1),
+        Album(4, 'Super Trouper', 1980, 2),
+        Album(5, 'Bossanova', 1990, 1),
+        Album(6, 'Lover', 2019, 3),
+        Album(7, 'Folklore', 2020, 3),
+        Album(8, 'I Put a Spell on You', 1965, 4),
+        Album(9, 'Baltimore', 1978, 4),
+        Album(10, 'Here Comes the Sun', 1971, 4),
+        Album(11, 'Fodder on My Wings', 1982, 4),
+        Album(12, 'Ring Ring', 1973, 2)
+    ]
